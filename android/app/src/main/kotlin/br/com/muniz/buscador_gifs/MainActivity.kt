fdaf990fad353a737251466a60faea81902dee20
William Muniz <william.muz@gmail.com>
2020-03-06 20:51:35 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/muniz/buscador_gifs/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.muniz.buscador_gifs
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:buscador_gifs/ui/gif.dart';
+import 'package:flutter/material.dart';
+import 'ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
